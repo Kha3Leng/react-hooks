d01bf08843988dd50464b652d204365725ea67cd
Easy Money <sai@Sai-MacBook-Pro.local>
2020-01-19 14:05:34 +0630

    useEffect()

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,23 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const ToDo = (props) => {
     const [todoName, setTodoName] = useState('');
     const [todoList, setTodoList] = useState([]);
+
+    useEffect(() => {
+        axios.get('https://to-do-81153.firebaseio.com/todo.json')
+            .then(res => {
+                console.log(res);
+                const todoData = res.data;
+                const todo = [];
+
+                for (const key in todoData) {
+                    todo.push({ id: key, name: todoData[key].name });
+                }
+                setTodoList(todo);
+            });
+    });
 
     const inputHandler = event => {
         setTodoName(event.target.value);
@@ -11,7 +25,7 @@ const ToDo = (props) => {
 
     const todoListHandler = _ => {
         setTodoList(todoList.concat(todoName));
-        axios.post('https://to-do-81153.firebaseio.com/todo.json', { todo: todoName })
+        axios.post('https://to-do-81153.firebaseio.com/todo.json', { name: todoName })
             .then(res => console.log(res))
             .catch(err => console.log(err));
     };
@@ -21,7 +35,7 @@ const ToDo = (props) => {
             <input type='text' placeholder="To Do" onChange={inputHandler} value={todoName} />
             <button type='button' onClick={todoListHandler}>Add</button>
             <ul>
-                {todoList.map(todo => <li key={todo}>{todo}</li>)}
+                {todoList.map(todo => <li key={todo.id}>{todo.name}</li>)}
             </ul>
         </div>
     );
