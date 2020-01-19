76447343d5770a4ae2a4a58fe60333cd7f27e9ab
Easy Money <sai@Sai-MacBook-Pro.local>
2020-01-19 13:32:00 +0630

    Revert "multiple useState() to one useState(): replacing"
    This reverts commit d12d291a2d96a763e40385301d90b09dda65ae83.

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,34 +1,23 @@
 import React, { useState } from 'react';
 
 const ToDo = (props) => {
-    // const [todoName, setTodoName] = useState('');
-    // const [todoList, setTodoList] = useState([]);
-
-    const [todoState, setTodoState] = useState({
-        inputName: '',
-        todoList: []
-    });
+    const [todoName, setTodoName] = useState('');
+    const [todoList, setTodoList] = useState([]);
 
     const inputHandler = event => {
-        setTodoState({
-            inputName: event.target.value,
-            todoList: todoState.todoList
-        });
+        setTodoName(event.target.value);
     };
 
     const todoListHandler = _ => {
-        setTodoState({
-            inputName: todoState.inputName,
-            todoList: todoState.todoList.concat(todoState.inputName)
-        });
+        setTodoList(todoList.concat(todoName));
     };
 
     return (
         <div>
-            <input type='text' placeholder="To Do" onChange={inputHandler} value={todoState.inputName} />
+            <input type='text' placeholder="To Do" onChange={inputHandler} value={todoName} />
             <button type='button' onClick={todoListHandler}>Add</button>
             <ul>
-                {todoState.todoList.map(todo => <li key={todo}>{todo}</li>)}
+                {todoList.map(todo => <li key={todo}>{todo}</li>)}
             </ul>
         </div>
     );
